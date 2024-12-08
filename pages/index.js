a6f303ca45929be5011d55f32acc539de0034040
chedas <106807276+pchedas@users.noreply.github.com>
2024-12-08 20:42:24 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 export default function Home() {
-  return (<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8"/>
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  return (
 <title>Book List Creator</title>
 <link rel="stylesheet" href="./styles.css">
 </head>
